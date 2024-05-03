afb8601e913ea80540d1893b5dc19c34f506434d
Collins Ngetich <collinsngetichk@gmail.com>
2024-05-03 22:30:43 +0300

    modifies components

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,13 @@
-// Code Keypad Component Here
-
-function Keypad (){
-    return (
-        <div></div>
-    )
+import React from "react";
+function Keypad() {
+  function handleChange() {
+    console.log("Entering password...");
+  }
+  return (
+    <div>
+      <input onChange={handleChange} type="password"></input>
+    </div>
+  );
 }
 
 export default Keypad;
